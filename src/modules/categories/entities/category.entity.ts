f40076be420bef7131ec0840ccf8c56cc0a3598d
Ramon Serrano <rameduardserrano@gmail.com>
2024-07-20 10:46:31 -0300

    Update category entity

--- src/modules/categories/entities/category.entity.ts
+++ src/modules/categories/entities/category.entity.ts
@@ -1,1 +1,34 @@
-export class Category {}
+import { Column, CreateDateColumn, Entity, Generated, PrimaryColumn, UpdateDateColumn } from 'typeorm';
+
+import { CategoryTypes } from './category.type';
+
+@Entity('categories')
+export class CategoryEntity {
+  @PrimaryColumn()
+  @Generated('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 1024 })
+  description: string;
+
+  @Column({ type: 'enum', enum: CategoryTypes })
+  type: CategoryTypes;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    precision: 3,
+    default: () => 'CURRENT_TIMESTAMP(3)',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    precision: 3,
+    default: () => 'CURRENT_TIMESTAMP(3)',
+    onUpdate: 'CURRENT_TIMESTAMP(3)',
+  })
+  updatedAt: Date;
+}
---
+++ src/modules/categories/entities/category.type.ts
@@ -0,0 +1,4 @@
+export enum CategoryTypes {
+  expense = 'expense',
+  income = 'income',
+}
