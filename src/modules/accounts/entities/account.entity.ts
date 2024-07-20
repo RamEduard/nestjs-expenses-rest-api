ce6aa1f6bfc7052d7949920b50f2f74f4247f86e
Ramon Serrano <rameduardserrano@gmail.com>
2024-07-20 10:43:42 -0300

    Update account entity

--- src/modules/accounts/entities/account.entity.ts
+++ src/modules/accounts/entities/account.entity.ts
@@ -1,1 +1,34 @@
-export class Account {}
+import { Column, CreateDateColumn, Entity, Generated, PrimaryColumn, UpdateDateColumn } from 'typeorm';
+
+import { AccountTypes } from './account.type';
+
+@Entity('accounts')
+export class AccountEntity {
+  @PrimaryColumn()
+  @Generated('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 1024 })
+  description: string;
+
+  @Column({ type: 'enum', enum: AccountTypes })
+  type: AccountTypes;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    precision: 3,
+    default: () => 'CURRENT_TIMESTAMP(3)',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    precision: 3,
+    default: () => 'CURRENT_TIMESTAMP(3)',
+    onUpdate: 'CURRENT_TIMESTAMP(3)',
+  })
+  updatedAt: Date;
+}
---
+++ src/modules/accounts/entities/account.type.ts
@@ -0,0 +1,5 @@
+export enum AccountTypes {
+  expense = 'expense',
+  income = 'income',
+  other = 'other',
+}
