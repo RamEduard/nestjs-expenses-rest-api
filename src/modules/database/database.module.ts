f672f1cd5ece35c2c81e26e64f1364f72c615276
Ramon Serrano <rameduardserrano@gmail.com>
2024-07-29 00:18:14 -0300

    Update database module with entities configured

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AccountEntity } from '../accounts/entities/account.entity';
+import { CategoryEntity } from '../categories/entities/category.entity';
+import { RecordEntity } from '../records/entities/record.entity';
+import { UserEntity } from '../users/entities/user.entity';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -10,7 +15,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'root',
       password: 'password',
       database: 'nestjs_expenses_dev',
-      entities: [],
+      entities: [AccountEntity, CategoryEntity, RecordEntity, UserEntity],
       synchronize: true,
     }),
   ],
