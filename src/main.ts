dacfbb7482433fc816531147ef9a6b74e9c0d98b
Ramon Serrano <rameduardserrano@gmail.com>
2024-08-18 14:44:26 -0300

    Add OpenApi with Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,20 @@ async function bootstrap() {
     }),
   );
 
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Expenses Manager')
+    .setDescription('The Open API for Expenses manager')
+    .setVersion('1.0')
+    .setExternalDoc('Swagger.json', '/api/swagger-json')
+    .addTag('expenses')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig, {
+    deepScanRoutes: true,
+    operationIdFactory: (_: string, methodKey: string) => methodKey,
+  });
+  SwaggerModule.setup('api/swagger', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
